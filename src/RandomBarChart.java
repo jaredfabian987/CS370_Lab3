e1b072b04e20fad336aa364bb4de7b6b01da31df
Jared Fabian <jarfabian987@gmail.com>
2026-02-09 13:52:16 -0800

    Got rid of the .form file

---
+++ src/RandomBarChart.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+// works cited/references
+// yt video : https://www.youtube.com/watch?v=Kmgo00avvEw
+public class RandomBarChart extends JFrame {
+
+    public RandomBarChart () {
+        // Set the window title
+        setTitle("Random BarChart");
+
+        // What happens when user clicks the X button
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Set the window size (width, height)
+        setSize(450, 550);
+
+        // Center the window on screen
+        setLocationRelativeTo(null);
+
+        // Make the window visible
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new RandomBarChart();
+    }
+}
